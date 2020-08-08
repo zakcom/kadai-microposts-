decd3fb8d1cee95466b6190fd55a8567eec3e0f1
NAO <zackzakumi62@outlook.jp>
2020-08-08 16:19:27 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -11,7 +11,7 @@
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                <li>{!! link_to_route('signup.get', 'signup',[], ['class' => 'nav-link']) !!}</li>
                 {{-- ログインページへのリンク --}}
                 <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
             </ul>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron"> 
-        <div class="text center">
-           <h1>Welcome to the Microposts</h1> 
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the Microposts</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class'=> 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
