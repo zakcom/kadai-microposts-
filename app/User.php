866f5d2d973db59d0359395cc254d6d90ca234e5
NAO <zackzakumi62@outlook.jp>
2020-08-16 05:58:02 +0000

    procfile

--- app/User.php
+++ app/User.php
@@ -116,6 +116,19 @@ class User extends Authenticatable
         // フォロー中ユーザの中に $userIdのものが存在するか
         return $this->followings()->where('follow_id', $userId)->exists();
     }
+    
+    /**
+     * このユーザとフォロー中ユーザの投稿に絞り込む。
+     */
+    public function feed_microposts()
+    {
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
+    }
 
     
      public function loadRelationshipCounts()
